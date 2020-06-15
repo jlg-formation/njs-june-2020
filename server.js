82b8283a5e0de95e9a610abc829563adf7cdf98d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-15 16:59:15 +0200

    added static middleware

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use((req, res, next) => {
+  console.log("req.url: ", req.url);
+  next();
+});
+
+app.use(express.static('www'));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
