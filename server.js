e8baef63ae78092f7112f529bcfb52217dbae89d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-16 14:52:34 +0200

    added dynamic article list

--- server.js
+++ server.js
@@ -2,17 +2,22 @@
 
 const express = require("express");
 const serveIndex = require("serve-index");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.use((req, res, next) => {
   console.log("req.url: ", req.url);
   next();
 });
 
+const articles = JSON.parse(fs.readFileSync("./database/articles.json"));
+
 app.get("/stock", (req, res, next) => {
-  res.sendFile("./tmpl/stock.html", { root: __dirname });
+  res.render("stock", { articles });
 });
 
 app.use(express.static("www"));
