e76820f8661a27e70b86738ff10b52ce24c6f250
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-17 11:35:37 +0200

    display supress button when needed

--- www/main.js
+++ www/main.js
@@ -1,8 +1,22 @@
+let selectedRowCounter = 0;
+
 function select(e) {
   console.log("select", e);
   if (e.classList.contains("selected")) {
     e.classList.remove("selected");
+    selectedRowCounter--;
   } else {
     e.classList.add("selected");
+    selectedRowCounter++;
+  }
+  console.log("selectedRowCounter: ", selectedRowCounter);
+
+  // decide if we need to display suppress button
+  const button = document.querySelector("button.button-delete");
+  console.log("button: ", button);
+  if (selectedRowCounter === 0) {
+    button.classList.add("jlg-hidden");
+  } else {
+    button.classList.remove("jlg-hidden");
   }
 }
