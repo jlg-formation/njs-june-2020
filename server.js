42041937d5488806afdb5fc03a280c538477ff7b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-18 11:15:24 +0200

    delete many

--- server.js
+++ server.js
@@ -75,10 +75,15 @@ app.delete("/webservices/bulk/articles", async (req, res, next) => {
   const ids = req.body;
   console.log("ids: ", ids);
 
+  const str = `DELETE FROM articles WHERE id IN (${ids
+    .map((n, i) => "$" + (i + 1))
+    .join(",")})`;
+
+  console.log("str: ", str);
+  await client.query(str, ids);
+
   try {
-    ids.forEach(async (id) => {
-      await client.query("DELETE FROM articles WHERE id = $1", [id]);
-    });
+    ids.forEach(async (id) => {});
     res.status(204).end();
   } catch (error) {
     console.log("err: ", err);
